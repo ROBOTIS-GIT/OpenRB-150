e104a0c1a507f6112508259ba990f38ccb9908d6
ROBOTIS-Will <willson@robotis.com>
2022-01-18 11:22:42 +0900

    wip

--- cores/arduino/USB/USBCore.cpp
+++ cores/arduino/USB/USBCore.cpp
@@ -71,11 +71,11 @@ const uint16_t STRING_LANGUAGE[2] = {
 
 const uint8_t STRING_PRODUCT[] = USB_PRODUCT;
 
-#if USB_VID == 0x2341
+#if USB_VID == 0xFFF1
 #  if defined(USB_MANUFACTURER)
 #    undef USB_MANUFACTURER
 #  endif
-#  define USB_MANUFACTURER "Arduino LLC"
+#  define USB_MANUFACTURER "ROBOTIS"
 #elif !defined(USB_MANUFACTURER)
 // Fall through to unknown if no manufacturer name was provided in a macro
 #  define USB_MANUFACTURER "Unknown"
